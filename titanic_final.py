aebf031bcc19ae5ac1c55eb42a015c0bf289e8e0
nahid15 <43151075+nahid15@users.noreply.github.com>
2019-05-22 16:49:58 +0600

    added accuracy test

--- titanic_final.py
+++ titanic_final.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import AdaBoostClassifier
-
+from sklearn.metrics import accuracy_score
 
 
 train = pd.read_csv('train.csv')
